3204c5e9ffda55ded0231162a4cb7e224514c950
Suman Bhadra <sumanbhadrasuman@gmail.com>
2023-08-13 23:12:52 +0400

    Fix Password link

--- Pages/password.py
+++ Pages/password.py
@@ -84,7 +84,7 @@ def password_generate_view():
             }}
 
             var encodedParameterValue = encodeURIComponent(parameterValue);
-            var url = "http://127.0.0.1:5000/generate_password?length=" + encodedParameterValue;
+            var url = "https://sumanbhadra.pythonanywhere.com/generate_password?length=" + encodedParameterValue;
 
             fetch(url)
                 .then(response => response.json())
