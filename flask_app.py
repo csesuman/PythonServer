860c22195a7f4dc501673ea2c01898aaa236b0ba
Suman Bhadra <sumanbhadrasuman@gmail.com>
2023-08-20 23:04:55 +0400

    Python log

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,14 @@
+import datetime
 import random
+import socket
 import string
+import uuid
 
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, json
 
 import Constants
 import requests
 # import pycountry
-
 
 
 import Utils
@@ -15,7 +17,7 @@ from Pages import home, reload, password, Clock
 app = Flask(__name__)
 
 
-@app.route('/') # start home page
+@app.route('/')  # start home page
 def hello_world():
     return home.home_view()
 
@@ -23,36 +25,21 @@ def hello_world():
 @app.route('/myip')
 def my_ip():
     x_forwarded_for = request.headers.getlist("X-Forwarded-For")
+    ip = x_forwarded_for[0]
+    url = f"http://ipinfo.io/{ip}/json"
+    response = requests.get(url)
+    data = response.json()
 
-    # url = f"http://ipinfo.io/{x_forwarded_for}/json"
-    # response = requests.get(url)
-
-    return x_forwarded_for
-    data = response
-
-    if "error" in data:
-        response = requests.get('https://ip-api.io/json')
-        data = response.json()
-        try:
-            ip_address = data['ip']
-            city = data['city']
-            region = data['region_name']
-            full_country_name = data['country_name']
-            return Utils.html_view(ip_address, city, region, full_country_name)
-        except AttributeError:
-            pass
-
-    else:
-        try:
-            ip_address = data['ip']
-            city = data['city']
-            region = data['region']
-            country_code = data['country']
-            # country = pycountry.countries.get(alpha_2=country_code) // fix ii
-            # full_country_name = country.name
-            return Utils.html_view(ip_address, city, region, country_code)
-        except AttributeError:
-            pass
+    try:
+        ip_address = data['ip']
+        city = data['city']
+        region = data['region']
+        country_code = data['country']
+        # country = pycountry.countries.get(alpha_2=country_code) // fix it
+        # full_country_name = country.name
+        return Utils.html_view(ip_address, city, region, country_code)
+    except AttributeError:
+        pass
 
 
 @app.route('/iplocation')
@@ -68,7 +55,8 @@ def get_public_ip_and_location():
     except Exception as e:
         return None, str(e)
 
-@app.route('/new_password', methods=['GET'])
+
+@app.route('/new_password', methods=['GET'])  # password generator
 def get_random_password_page():
     return password.password_generate_view()
 
@@ -79,8 +67,8 @@ def get_clock_page():
     return Clock.digital_clock_view()
 
 
-@app.route('/echo', methods=['POST','GET'])
-@app.route('/webhook', methods=['POST','PUT','GET','PATCH','DELETE'])
+@app.route('/echo', methods=['POST', 'GET'])
+@app.route('/webhook', methods=['POST', 'PUT', 'GET', 'PATCH', 'DELETE'])
 def webhook_echo_responder():
     data = request.data.decode('utf-8')
     args = dict(request.args)
@@ -106,6 +94,34 @@ def call_self():
     global hit
     hit = hit + 1
     return reload.reload_view(hit)
+
+
+def internal_details():
+    method = request.method
+    url = request.url
+    headers = dict(request.headers)
+    data = request.data.decode('utf-8')
+    args = dict(request.args)
+    form = dict(request.form)
+    # Verify the password
+
+    # Extract the password from the request headers
+    passkey = headers.get('Password')
+
+    response = {
+        "message": "Request details fetched successfully!",
+        "details": {
+            "Request Method": method,
+            "URL": url,
+            "Headers": headers,
+            "Raw Data": data,
+            "Query Parameters": args,
+            "Form Data": form
+        }
+    }
+
+
+    return response
 
 
 @app.route('/details', methods=['GET', 'POST'])
@@ -137,6 +153,72 @@ def request_details():
     }
 
     return jsonify(response)
+
+
+@app.route('/index')
+def index():
+    details = internal_details()
+    json_string = json.dumps(details)
+
+    current_time = datetime.datetime.now()
+    unique_id = uuid.uuid4()
+
+    with open('saved_text.txt', 'a') as file:
+        file.write(str(unique_id) + " >> " + str(current_time) + json_string + '\n')
+
+    # return json_string
+    return render_template('input_message.html', unique_id=unique_id)
+
+
+@app.route('/save', methods=['POST'])
+def save_text():
+    try:
+        data = request.get_json()
+        text = data['text']
+
+        with open('saved_text.txt', 'a') as file:
+            file.write(text + '\n')
+
+        # with open('saved_text.txt', 'a') as file:
+        #     json.dump(details, file)
+        #     file.write('\n' + text + '\n')
+
+        return jsonify(message='Text saved to server successfully!')
+    except Exception as e:
+        return jsonify(error=str(e)), 500
+
+
+@app.route('/make_request', methods=['POST'])
+def make_request():
+    try:
+        data = request.get_json()
+        url = data.get('url')
+        request_type = data.get('request_type', 'GET')
+        counter = data.get('counter', 1)
+
+        if not url:
+            return jsonify({'error': 'URL is required'}), 400
+
+        response_data = []
+
+        for _ in range(counter):
+            if request_type.upper() == 'GET':
+                response = requests.get(url)
+            elif request_type.upper() == 'POST':
+                response = requests.post(url)
+            else:
+                return jsonify({'error': 'Unsupported request type'}), 400
+
+            response_info = {
+                'status_code': response.status_code,
+                'content': response.text
+            }
+            response_data.append(response_info)
+
+        return jsonify(response_data), 200
+    except Exception as e:
+        error_message = str(e)
+        return jsonify({'error': error_message}), 500
 
 
 def generate_password(length=12):
